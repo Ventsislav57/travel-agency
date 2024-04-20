734b3e9c295c7ac2aa4f6a355859633a12b5528e
Ventsislav57 <Ventsislav57>
2024-04-20 17:12:23 +0300

    Add animate for Good Service

--- client/src/Componenets/GoodService_2/GoodService.js
+++ client/src/Componenets/GoodService_2/GoodService.js
@@ -1,18 +1,34 @@
+import { useEffect, useState } from 'react';
 import { motion } from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
 
+import { animatePropsForText, animatePropsForBigCircle, animatePropsForImg, animatePropsForSmallCircle } from './animateProps.js';
 import styles from './GoodService.module.css';
 
 export const GoodService = () => {
 
+    const [ ref, inView ] = useInView();
+    const [ isInView, setIsInView ] = useState('hidden');
 
+    useEffect(() => {
+
+        if (inView) {
+            setIsInView('visible');
+        } else {
+            setIsInView('hidden');
+        }
+
+    }, [inView]);
+ 
 
     return (
         <section className={styles['good_service']}>
             
             <motion.div 
-                initial={{ x: '-500px' }}
-                animate={{ x: '0' }}
-                transition={{ duration: 1 }}
+                ref={ref}
+                initial="hidden"
+                animate={isInView}
+                variants={animatePropsForText}
                 className={styles['title_subtitle_btn']}
             >
                 <h1>Good Trips Only</h1>
@@ -29,12 +45,32 @@ export const GoodService = () => {
 
             <div className={styles['img_container']}>
 
-                <div className={styles['big_circle']}></div>
+                <motion.div 
+                    ref={ref}
+                    initial="hidden"
+                    animate={isInView}
+                    variants={animatePropsForBigCircle}                   
+                    className={styles['big_circle']}
+                ></motion.div>
+
                 <div className={styles['img']}>
-                    <img src='./good_service-modified.png' alt='pictures' />
+                    <motion.img 
+                        src='./good_service-modified.png' alt='pictures' 
+                        ref={ref}
+                        initial="hidden"
+                        animate={isInView}
+                        variants={animatePropsForImg}  
+                    >
+                    </motion.img>
                 </div>
-                <div className={styles['small_circle']}></div>
-                
+
+                <motion.div 
+                    ref={ref}
+                    initial="hidden"
+                    animate={isInView}
+                    variants={animatePropsForSmallCircle}                   
+                    className={styles['small_circle']}
+                ></motion.div>
             </div>
 
         </section>
---
+++ client/src/Componenets/GoodService_2/animateProps.js
@@ -0,0 +1,19 @@
+export const animatePropsForText = {
+    visible: { x: '0px', transition: { duration: 1 } },
+    hidden:  { x: '-400px' }
+}
+
+export const animatePropsForBigCircle = {
+    visible: { scale: 1, transition: { duration: 1 } },
+    hidden: { scale: 0 }
+}
+
+export const animatePropsForImg = {
+    visible: { scale: 1, rotate: 0, transition: { duration: 1 } },
+    hidden: { scale: 0, rotate: -120 }
+}
+
+export const animatePropsForSmallCircle = {
+    visible: { scale: [0, 1.7, 1], transition: { duration: 1 } },
+    hidden: { scale: 0 }
+}
