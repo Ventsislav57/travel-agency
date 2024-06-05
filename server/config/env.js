a30beecf7d55f04d3f193c14aa652fb80c515199
Ventsislav57 <Ventsislav57>
2024-06-05 20:32:44 +0300

    Add mongoDB connection string

--- server/config/env.js
+++ server/config/env.js
@@ -1,4 +1,9 @@
 exports.port = 3050;
 exports.saltRounds = 10;
 exports.secret = 'asdkj234ipj3490urv3joip3g4;lmwcerv0-234fl324c';
-exports.connectionString = 'mongodb://localhost:27017/travel-agency';
+
+// Local
+// exports.connectionString = 'mongodb://localhost:27017/travel-agency';
+
+// Network
+exports.connectionString = 'mongodb+srv://tenak:0B17!a12v@travel-agency.c9t4igt.mongodb.net/?retryWrites=true&w=majority&appName=Travel-Agency'
